af18fffecfd6cbd692db286597f3e143739d406e
RealLionelMuhire <muhirelionel@gmail.com>
2023-08-26 11:36:17 +0200

    about importing

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 
 db_connect_str = "mysql+pymysql://w9947rumlo8lkhsvho06:pscale_pw_iddkxaScHFdWZTCzPeRHCJNtOJHhNq2zUZ6CoAuPWfP@aws.connect.psdb.cloud/ml_career?charset=utf8mb4"
 
