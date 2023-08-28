8c070d8efb06db09e0439965d4a9ea7f4b45921e
RealLionelMuhire <103781290+RealLionelMuhire@users.noreply.github.com>
2023-08-28 18:00:22 +0000

    modifying connection in aws

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from sqlalchemy import create_engine, text
 
-db_connect_str = "mysql+pymysql://w9947rumlo8lkhsvho06:pscale_pw_iddkxaScHFdWZTCzPeRHCJNtOJHhNq2zUZ6CoAuPWfP@aws.connect.psdb.cloud/ml_career?charset=utf8mb4"
+db_connect_str = "mysql+pymysql://u46f7jz7673tgk7ejewi:pscale_pw_meb5CG2GCsHJH8spxW1PYxV9AWXnQcJJQ5KiZ5IAKGq@aws.connect.psdb.cloud/ml_career?charset=utf8mb4"
 
 engine = create_engine(
     db_connect_str,
